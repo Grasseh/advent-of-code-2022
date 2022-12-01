b51924523ca2518f9f001c0a2d46cf0c6be9331d
Steve Gagné <steve.gagne@nasdaq.com>
2022-12-01 17:26:54 -0500

    Day 1: Calorie Counting - Cleanup + Benchmark

--- solvers/solver_1.rb
+++ solvers/solver_1.rb
@@ -4,23 +4,19 @@ require 'active_support/core_ext'
 module Solvers
   class Solver1
     def solve_a(input, _opts = {})
-      elves = input.split("\n")
-
-      calories = elves.map do |elf|
-        elf.map(&:chomp).map(&:to_i).sum
-      end
-
-      calories.max
+      get_calories(input).max
     end
 
     def solve_b(input, _opts = {})
+      get_calories(input).sort.last(3).sum
+    end
+
+    def get_calories(input)
       elves = input.split("\n")
 
-      calories = elves.map do |elf|
+      elves.map do |elf|
         elf.map(&:chomp).map(&:to_i).sum
       end
-
-      calories.sort.last(3).sum
     end
   end
 end
