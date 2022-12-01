359eb10fef4808dcbdb17ec4bc41f140d184a6e8
Steve Gagné <steve.gagne@nasdaq.com>
2022-12-01 15:21:02 -0500

    Day 1: Calorie Counting - Part one

---
+++ solvers/solver_1.rb
@@ -0,0 +1,20 @@
+require 'active_support'
+require 'active_support/core_ext'
+
+module Solvers
+  class Solver1
+    def solve_a(input, _opts = {})
+      elves = input.split("\n")
+
+      calories = elves.map do |elf|
+        elf.map(&:chomp).map(&:to_i).sum
+      end
+
+      calories.max
+    end
+
+    def solve_b(input, _opts = {})
+      -1
+    end
+  end
+end
---
+++ test/solver_1_test.rb
@@ -0,0 +1,38 @@
+require_relative '../solvers/solver_1'
+require_relative 'test_helper'
+
+module Tests
+  class Solver1Test < Minitest::Test
+    def setup
+      @solver = Solvers::Solver1.new
+    end
+
+    def example_a
+      [
+        '1000',
+        '2000',
+        '3000',
+        '',
+        '4000',
+        '',
+        '5000',
+        '6000',
+        '',
+        '7000',
+        '8000',
+        '9000',
+        '',
+        '10000'
+      ]
+    end
+
+    def test_problem_a
+      expected = 24000
+
+      assert_equal(
+        expected,
+        @solver.solve_a(wrap_array_input(*example_a))
+      )
+    end
+  end
+end
