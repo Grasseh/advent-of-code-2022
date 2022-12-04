6d1729d0a2a3583d19a3f23381b804c1151106fe
Steve Gagné <steve.gagne@nasdaq.com>
2022-12-04 02:34:04 -0500

    Add slower optional cuter solution for Day 4

--- solvers/solver_4.rb
+++ solvers/solver_4.rb
@@ -33,5 +33,38 @@ module Solvers
 
       overlap.count
     end
+
+    def solve_a_cute_but_slow(input, _opts = {})
+      overlap = input.select do |elf_pair|
+        first_elf, second_elf = elf_pair.chomp.split(',')
+        first_elf_start, first_elf_end = first_elf.split('-').map(&:to_i)
+        second_elf_start, second_elf_end = second_elf.split('-').map(&:to_i)
+        first_elf_range = first_elf_start..first_elf_end
+        second_elf_range = second_elf_start..second_elf_end
+
+        intersection = first_elf_range.to_a.intersection(second_elf_range.to_a)
+
+        intersection.count == first_elf_range.count ||
+          intersection.count == second_elf_range.count
+      end
+
+      overlap.count
+    end
+
+    def solve_b_cute_but_slow(input, _opts = {})
+      overlap = input.select do |elf_pair|
+        first_elf, second_elf = elf_pair.chomp.split(',')
+        first_elf_start, first_elf_end = first_elf.split('-').map(&:to_i)
+        second_elf_start, second_elf_end = second_elf.split('-').map(&:to_i)
+        first_elf_range = first_elf_start..first_elf_end
+        second_elf_range = second_elf_start..second_elf_end
+
+        intersection = first_elf_range.to_a.intersection(second_elf_range.to_a)
+
+        intersection.count.positive?
+      end
+
+      overlap.count
+    end
   end
 end
