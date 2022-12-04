ae6c677550eebadc97791371eb9de4d652cfe457
Steve Gagné <steve.gagne@nasdaq.com>
2022-12-04 02:18:07 -0500

    Day 4: Camp Cleanup - Part One

---
+++ solvers/solver_4.rb
@@ -0,0 +1,26 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'pry'
+
+module Solvers
+  class Solver4
+    def solve_a(input, _opts = {})
+      overlap = input.select do |elf_pair|
+        first_elf, second_elf = elf_pair.chomp.split(',')
+        first_elf_start, first_elf_end = first_elf.split('-').map(&:to_i)
+        second_elf_start, second_elf_end = second_elf.split('-').map(&:to_i)
+
+        (
+          first_elf_start <= second_elf_start && first_elf_end >= second_elf_end
+        ) ||
+          first_elf_start >= second_elf_start && first_elf_end <= second_elf_end
+      end
+
+      overlap.count
+    end
+
+    def solve_b(_input, _opts = {})
+      -1
+    end
+  end
+end
