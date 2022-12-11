b210664cfcea0d256d68ab3bd617baebea369795
Steve Gagné <steve.gagne@nasdaq.com>
2022-12-11 00:40:17 -0500

    Day 10: Cathode-Ray Tube - Part Two

--- solvers/solver_10.rb
+++ solvers/solver_10.rb
@@ -4,36 +4,62 @@ require 'pry'
 
 module Solvers
   class Solver10
-    def solve_a(input, _opts = {})
-      cycle_count = 0
-      register = 1
-      signal_strength = 0
+    def initialize
+      @screen = ''
+      @cycle_count = 0
+      @register = 1
+      @signal_strength = 0
+    end
 
+    def solve_a(input, _opts = {})
+      run_program(input)
+      @signal_strength
+    end
+
+    def solve_b(input, _opts = {})
+      run_program(input)
+      @screen.chars.each_slice(40).map(&:join).join("\n")
+    end
+
+    def run_program(input)
       input.each do |row|
         operation, value = row.chomp.split
 
         if operation == 'noop'
-          value = 0
-          cycle_count += 1
+          screen_loop(1, 0)
         else
-          cycle_count += 2
-          register += value.to_i
-
-          if ((cycle_count - 21) % 40).zero?
-            signal_strength += (register - value.to_i) * (cycle_count - 1)
-          end
-        end
-
-        if ((cycle_count - 20) % 40).zero?
-          signal_strength += (register - value.to_i) * cycle_count
+          screen_loop(2, value.to_i)
         end
       end
-
-      signal_strength
     end
 
-    def solve_b(_input, _opts = {})
-      -1
+    def screen_loop(execution_time, add_value)
+      execution_time.times do
+        @cycle_count += 1
+        draw_screen
+        calculate_signal_strength
+      end
+
+      @register += add_value
+    end
+
+    def draw_screen
+      @screen += if pixel_display
+        '#'
+      else
+        '.'
+      end
+    end
+
+    def pixel_display
+      (@register..(@register + 2)).include?(@cycle_count % 40) ||
+        (@cycle_count % 40).zero? && (38..40).include?(@register)
+    end
+
+    def calculate_signal_strength
+      return unless ((@cycle_count - 20) % 40).zero?
+
+      @signal_strength += @register * @cycle_count
     end
   end
 end
